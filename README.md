6b3269c58a3beccceab019e88d902bdf2a652b68
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2024-11-30 19:41:30 -0500

    initial functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # jqedit
+
+```bash
+go install github.com/myaaaaaaaaa/jqedit@latest
+```
+
+## Special Thanks
+
+* https://github.com/jqlang/jq/
+* https://github.com/itchyny/gojq/
+* https://github.com/charmbracelet/bubbletea
---
+++ main.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/myaaaaaaaaa/go-jqx"
+)
+
+const sampleJSON = `{"a":5,"b":"c","d":["e","f"],"g":{"h":"i","j":"k"}}`
+
+var spaceRe = regexp.MustCompile(`\S+`)
+
+func fmtScript(code string) string {
+	words := spaceRe.FindAllString(code, -1)
+	code = strings.Join(words, " ")
+	if code == "" {
+		code = "."
+	}
+	return code
+}
+func query(code, input string) (string, error) {
+	var output bytes.Buffer
+
+	prog := jqx.Program{
+		Args:   []string{" " + code},
+		Stdin:  bytes.NewBufferString(input),
+		Stdout: &output,
+
+		StdoutIsTerminal: true,
+	}
+	err := prog.Main()
+	rt := output.String()
+	if rt == "null\n" {
+		return rt, fmt.Errorf("error: query returned null")
+	}
+	return rt, err
+}
+
+type model struct {
+	textarea textarea.Model
+	viewport viewport.Model
+	input    string
+	code     string
+	err      error
+	num      int
+}
+
+func newModel(text string) model {
+	rt := model{
+		textarea: textarea.New(),
+		viewport: viewport.New(80, 15),
+		input:    text,
+		code:     "#placeholder",
+	}
+
+	rt.textarea.SetWidth(60)
+	rt.textarea.SetHeight(3)
+	rt.textarea.Placeholder = "Once upon a time..."
+	rt.textarea.Focus()
+
+	return rt
+}
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return updateMsg(func(m *model) tea.Cmd {
+			m.num++
+			return tick()
+		})
+	})
+}
+
+var logged = map[string]bool{}
+
+func logScript(code string) tea.Cmd {
+	if logged[code] {
+		return nil
+	}
+	logged[code] = true
+	return tea.Printf("    '%s'", code)
+}
+
+type updateMsg func(*model) tea.Cmd
+type switchMsg struct {
+	m tea.Model
+	c tea.Cmd
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		textarea.Blink,
+		tick(),
+	)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case updateMsg:
+		cmd := msg(&m)
+		return m, cmd
+	case switchMsg:
+		return msg.m, msg.c
+	case tea.MouseMsg:
+		var cmd tea.Cmd
+		m.viewport, cmd = m.viewport.Update(msg)
+		return m, cmd
+	default:
+		var cmd tea.Cmd
+		var text string
+		m.textarea, cmd = m.textarea.Update(msg)
+		code := fmtScript(m.textarea.Value())
+		if m.code == code {
+			goto abortUpdate
+		}
+
+		m.code = code
+		text, m.err = query(code, m.input)
+		if m.err != nil {
+			goto abortUpdate
+		}
+
+		cmd = tea.Batch(cmd, logScript(code))
+		m.viewport.SetContent(text)
+
+	abortUpdate:
+		return m, cmd
+	}
+
+	//return m, nil
+}
+
+var headerStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#4444cc"))
+var errorStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#880000"))
+
+func (m model) View() string {
+	viewport := m.viewport.View()
+
+	err := ""
+	if m.err != nil {
+		err = m.err.Error()
+	}
+
+	tock := "Tick"
+	if m.num%2 == 1 {
+		tock = "Tock"
+	}
+	tock = fmt.Sprint(tock, " #", m.num)
+
+	return lipgloss.JoinVertical(lipgloss.Center,
+		headerStyle.Render(tock),
+		strings.Repeat("─", 5),
+
+		lipgloss.JoinHorizontal(lipgloss.Top,
+			"    ",
+			viewport,
+		),
+
+		strings.Repeat("─", 5),
+		m.textarea.View(),
+	) + "\n" + errorStyle.Render(err)
+}
+
+func msgFilter(_ tea.Model, msg tea.Msg) tea.Msg {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc, tea.KeyCtrlC, tea.KeyCtrlQ:
+			return switchMsg{
+				emptyModel{},
+				tea.Quit,
+			}
+		}
+	}
+	return msg
+}
+
+type emptyModel struct{}
+
+func (_ emptyModel) Init() tea.Cmd                         { return nil }
+func (e emptyModel) Update(_ tea.Msg) (tea.Model, tea.Cmd) { return e, nil }
+func (_ emptyModel) View() string                          { return "" }
+
+func isTerminal(f fs.File) bool {
+	stat, err := f.Stat()
+	if err != nil {
+		return false
+	}
+	return stat.Mode()&fs.ModeCharDevice != 0
+}
+func main() {
+	text := sampleJSON
+	if !isTerminal(os.Stdin) {
+		b, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		text = string(b)
+	}
+
+	p := tea.NewProgram(newModel(text),
+		tea.WithFilter(msgFilter),
+		tea.WithMouseCellMotion(),
+	)
+
+	if _, err := p.Run(); err != nil {
+		panic(err)
+	}
+}
